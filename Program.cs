baaec9ed835edb8d29d1b357b4265d057a740df1
rodrigocitadin <citadinrodrigo@gmail.com>
2023-10-25 09:04:05 -0300

    uhul

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.NetworkInformation;
+using System.Text;
+
+Ping pingSender = new();
+PingOptions options = new();
+
+options.DontFragment = true;
+
+string data = "testing";
+byte[] buffer = Encoding.ASCII.GetBytes(data);
+int timeout = 150;
+string address = "4.2.2.2";
+
+
+PingReply reply = pingSender.Send(address, timeout, buffer, options);
+
+if (reply.Status == IPStatus.Success)
+{
+    Console.WriteLine($"Response: {reply.Status.ToString()}");
+    Console.WriteLine($"RoundTrip: {reply.RoundtripTime}");
+    Console.WriteLine($"Time to live: {reply.Options.Ttl}");
+    Console.WriteLine($"Buffer size: {reply.Buffer.Length}");
+}
+else
+{
+    Console.WriteLine("Error");
+}
